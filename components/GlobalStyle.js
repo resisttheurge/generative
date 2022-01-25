56491af4b4801b027629e0728b7fb7adcf84bc33
Peter Zastoupil <peterzastoupil@gmail.com>
2022-01-24 20:44:09 -0600

    added grids simulation

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components'
 
+import 'normalize.css'
+import 'material-icons/iconfont/material-icons.css'
+
 const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+*, *:before, *:after {
+  box-sizing: inherit;
+}
 `
 
 export default GlobalStyle
--- pages/glsl.js
+++ pages/glsl.js
@@ -2,6 +2,8 @@ import { vec2 } from 'gl-matrix'
 
 import Sketch from '../components/Sketch'
 import WebGLCanvas from '../components/WebGLCanvas'
+import { loadShader, initializeProgram } from '../lib/webgl'
+
 const vert = x => x
 const frag = vert
 
@@ -30,7 +32,6 @@ const GLSL = () => (
         gl.clearColor(0.0, 0.0, 0.0, 1.0)
         // Clear the color buffer with specified clear color
         gl.clear(gl.COLOR_BUFFER_BIT)
-        console.log(vec2(1, 2) * vec2(3, 4))
       }}
     />
   </Sketch>
